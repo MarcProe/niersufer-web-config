da0fae1caaaeae3e8169a240acb52945f89ab9b9
Marcus Proest <marcus@proest.net>
2018-03-10 22:40:23 +0100

    add nginx

--- collect.sh
+++ collect.sh
@@ -2,4 +2,4 @@ cp /etc/prometheus/prometheus.yml prometheus
 cp /etc/influxdb/influxdb.conf influxdb
 cp /var/www/html/config.json hopglass
 cp /opt/hopglass-server/config.json hopglass-server
-
+cp /etc/nginx/sites-enabled/default nginx
