a60d2baf065d5efc8c27cf9ce55df5be73f413ae
Marcus Proest <marcus@proest.net>
2018-03-11 08:49:58 +0100

    add upstart scripts

--- collect.sh
+++ collect.sh
@@ -1,5 +1,8 @@
 cp /etc/prometheus/prometheus.yml prometheus
+cp /etc/systemd/system/prometheus.service prometheus
+cp  /etc/systemd/system/node_exporter.service prometheus
 cp /etc/influxdb/influxdb.conf influxdb
+cp /etc/systemd/system/influxd.service influxdb
 cp /var/www/html/config.json hopglass
 cp /opt/hopglass-server/config.json hopglass-server
 cp /etc/nginx/sites-enabled/default nginx
